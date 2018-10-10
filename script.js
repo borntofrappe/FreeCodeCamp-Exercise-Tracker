cbc0b2efb1ae565b1e5f60fa614253f66988e2a8
gabrielecorti <borntofrappe@gmail.com>
2018-10-10 23:23:10 +0200

    include body-parser to contemplate the data

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 // set up an express app
 const port = 3000;
+
+/* require the following packages:
+- express
+- body-parser
+*/
 const express = require('express');
+const bodyParser = require('body-parser');
+
+// set up an express app
 const app = express();
+
+// mount the body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // render the stylesheet as found in the public folder
 app.use(express.static(`${__dirname}/public`));
@@ -10,6 +21,26 @@ app.use(express.static(`${__dirname}/public`));
 // in the root path render the HTML file as found in the views folder
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
+});
+
+// in the path selected to register a user, retrieve the value of the username and register the user
+// ! first look for an existing entry, in which case return a JSON object detailing the occurrence
+app.post('/api/exercise/new-user', function (req, res) {
+  const { username } = req.body;
+  res.send(username);
+});
+
+// in the path selected to register an exercise, retrieve the values for the userId, description, duration and date
+app.post('/api/exercise/add', function (req, res) {
+  const { userId, description, duration, dateYear, dateMonth, dateDay } = req.body;
+  res.send(req.body);
+});
+
+// in the path selected to log the excercises, return the data attached to the userId
+// ! if existing, otherwise return a JSON object detailing the occurrence
+// ! currently return a hard-coded message as to test the method
+app.get('/api/exercise/log', function (req, res) {
+  res.send(req.query);
 });
 
 

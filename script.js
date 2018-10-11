c417bdd02993ff7abe5dbf6d80842054b8ee5de6
gabrielecorti <borntofrappe@gmail.com>
2018-10-11 23:32:07 +0200

    add the functionality to register an exercise

--- script.js
+++ script.js
@@ -93,10 +93,55 @@ app.post('/api/exercise/new-user', function (req, res) {
   });
 });
 
-// in the path selected to register an exercise, retrieve the values for the userId, description, duration and date
+// in the path selected to register an exercise, find and update the document matching the id
 app.post('/api/exercise/add', function (req, res) {
-  const { userId, description, duration, dateYear, dateMonth, dateDay } = req.body;
-  res.send(req.body);
+  // retrieve the values from the form
+  const { userId: _id, description, duration, dateYear, dateMonth, dateDay } = req.body;
+  // create an instance of the date object, based on the year, month and day value
+  const date = new Date(`${dateYear}-${dateMonth}-${dateDay}`);
+
+  // create a log out of the description, duration and date fields
+  const log = {
+    description,
+    duration,
+    date
+  };
+
+  // look for a document with a matching _id value
+  User.findOneAndUpdate({
+    _id
+  },
+    {
+      // push in the log array the new object detailing the exercise
+      $push: {
+        log
+      }
+    },
+    {
+      // in the options set new to be true, as to have the function return the updated document
+      new: true
+    }, (errFound, userFound) => {
+      if (errFound) {
+        console.log("findOne() error");
+      }
+      // findOneAndUpdate returns **null** or a matching **document** depending on whether a match is found
+      if (userFound) {
+        // if a match is found, return a json object detailing the username and latest exercise
+        const { username } = userFound;
+        res.json({
+          _id,
+          username,
+          description,
+          duration,
+          date: date.toDateString()
+        });
+      }
+      else {
+        // findOne returns null, detail the occurrence
+        res.send("unknown _id");
+      }
+    })
+
 });
 
 // in the path selected to log the excercises, return the data attached to the userId
